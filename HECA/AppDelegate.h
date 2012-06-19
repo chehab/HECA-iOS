fcb43c41436b4940df9d57a2deeb6d983c085fcb
Chehab Mustafa-Hilmy <m@chehab.me>
2012-06-19 10:34:28 +0200

    Initial Commit

---
+++ HECA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HECA
+//
+//  Created by Chehab Mustafa-Hilmy on 6/19/12.
+//  Copyright (c) 2012 Freelance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HECA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HECA
+//
+//  Created by Chehab Mustafa-Hilmy on 6/19/12.
+//  Copyright (c) 2012 Freelance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
